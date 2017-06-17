5b9e0a561f22732ee4d2677c3dc01bf4a4723e97
Chad Jemmett <chad.jemmett@gmail.com>
2017-06-16 19:45:25 -0600

    wave method works for enemies

--- enemy.rb
+++ enemy.rb
@@ -9,6 +9,9 @@ class Enemy
     @image = Gosu::Image.new("media/enemy.png")
     @message = Gosu::Font.new(15)
     @window = window
+    @top_of_wave = @y - @image.height
+    @bottom_of_wave = @y + @image.height
+    @going_up = true
   end
 
   def move
@@ -21,6 +24,18 @@ class Enemy
   end
 
   def wave
+    if @going_up == true and @y >= @top_of_wave
+      @y -= 3
+    end
+    if @y <= @top_of_wave
+      @going_up = false
+    end
+    if @going_up == false and @y <= @bottom_of_wave
+      @y += 3
+      end
+    if @y >= @bottom_of_wave
+      @going_up = true
+      end
   end
 
   def veer
